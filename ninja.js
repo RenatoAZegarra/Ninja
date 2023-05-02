9868b823a18a4f1e049d83ea36360bf4f642d054
RenatoAZegarra <Zegarrarenatoa@gmail.com>
2023-05-02 16:59:24 -0400

    first commit

---
+++ ninja.js
@@ -0,0 +1,22 @@
+class Ninja {
+  constructor(name){
+    this.name = name;
+    this.health = 100;
+    this.speed = 3;
+    this.strength = 3;
+  }
+  sayName(){
+    console.log( ` ${this.name} ` )
+  }
+  showStats() {
+    console.log(`Name: ${this.name} Speed: ${this.speed} Strength: ${this.strength} Health: ${this.health}`)
+  }
+  drinkSake() {
+    this.health += 10;
+  }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.drinkSake();
+ninja1.showStats();
